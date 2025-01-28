a14785f12ba5ec9f742317b4d4861a0f991572cb
재영 <yim020327@gmail.com>
2025-01-28 15:26:58 +0900

    [level 0] Title: 배열 만들기 4, Time: 54.81 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,29 +1,29 @@
-import java.util.ArrayList;
+import java.util.Stack;
 
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> s = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
         
         for(int i = 0; i < arr.length; i++){
-            if(s.size() == 0){ // 1. 빈 배열
-                s.add(arr[i]);
+            if(stk.empty() == true){
+                stk.push(arr[i]);
             }
             else{
-                if(s.get(s.size()-1) < arr[i]){ // 2. 값 있고 작을 때
-                    s.add(arr[i]);
+                if(stk.peek() < arr[i]){
+                    stk.push(arr[i]);
                 }
-                else{ // 3. 값 있고 크거나 같을 때
-                    s.remove(s.size()-1);
+                else{
+                    stk.pop();
                     i--;
                 }
             }
-        } 
-        
-        int[] stk = new int[s.size()];
-        for(int j = 0; j < s.size(); j++){
-            stk[j] = s.get(j).intValue();
         }
         
-        return stk;
+        int[] result = new int[stk.size()];
+        for(int j = stk.size() - 1; j >= 0; j--){
+            result[j] = stk.pop();
+        }
+        
+        return result;
     }
 }
