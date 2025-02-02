038c47d2630c49da421942adf1d3168a4adc0f21
재영 <yim020327@gmail.com>
2025-02-02 20:18:34 +0900

    [level 0] Title: qr code, Time: 20.71 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -2,10 +2,8 @@ class Solution {
     public String solution(int q, int r, String code) {
         String answer = "";
         
-        for(int i = 0; i < code.length(); i++){
-            if(i % q == r){
-                answer += code.charAt(i);
-            }
+        for(int i = r; i < code.length(); i+=q){
+            answer += code.charAt(i);
         }
         
         return answer;
