29babe1c2e588cf118075b6bdeb6680c46d45469
재영 <yim020327@gmail.com>
2025-02-02 19:23:11 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if(my_string.substring(0,i).equals(is_prefix)){
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+}
