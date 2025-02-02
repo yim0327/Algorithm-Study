0603e6cd25438aeedd97356fcf608152efd4b169
재영 <yim020327@gmail.com>
2025-02-03 01:25:10 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.36 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -2,34 +2,26 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int l, int r) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int t = 0;
-        
-        int[] arr = {5, 50, 55, 500, 505, 550, 555, 5000, 5005,5050,5055,5500,
-                    5505, 5550, 5555, 50000, 50005, 50050, 50055, 50500, 50505,
-                     50550,50555, 55000, 55005, 55050, 55055, 55500, 55505,
-                     55550, 55555, 500000, 500005, 500050, 500055, 500500, 
-                     500505, 500550, 500555, 505000, 505005, 505050, 505055,
-                    505500, 505505, 505550, 505555, 550000, 550005, 550050,
-                    550055, 550500, 550505, 550550, 550555, 555000, 555005,
-                    555050, 555055, 555500, 555505, 555550, 555555};
-        
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] >= l && arr[i] <= r){
-                answer.add(arr[i]);
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i = 1; i < 64; i++){ // i = 규칙과 맞아 떨어지는 이진수
+            String bs = Integer.toBinaryString(i);
+            int num = Integer.parseInt(bs) * 5; // 실제 비교를 위한 정수
+            if(num >= l && num <= r){
+                arr.add(num);
             }
         }
-        
-        if(answer.size() == 0){
-            answer.add(-1);
+
+        if(arr.isEmpty()){
+            arr.add(-1);
         }
         
-        int[] tmp = new int[answer.size()];
+        int[] answer = new int[arr.size()];
         
-        for(int j = 0; j < answer.size(); j++){
-            tmp[j] = answer.get(j).intValue();
+        for(int j = 0; j < arr.size(); j++){
+            answer[j] = arr.get(j).intValue();
         }
         
-        return tmp;
+        return answer;
     }
 }
