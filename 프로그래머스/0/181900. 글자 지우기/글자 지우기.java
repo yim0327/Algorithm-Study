1b4c7afba65995ed658479093ee1c07f656febb7
재영 <yim020327@gmail.com>
2025-02-03 18:32:29 +0900

    [level 0] Title: 글자 지우기, Time: 3.66 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 class Solution {
     public String solution(String my_string, int[] indices) {
         String answer = "";
-        char[] st = my_string.toCharArray();
+        String[] st = my_string.split("");
         
         for(int i = 0; i < indices.length; i++){
-            st[indices[i]] = ' ';
+            st[indices[i]] ="";
         }
         
         for(int j = 0; j < my_string.length(); j++){
             answer += st[j];
         }
         
-        return answer.replace(" ","");
+        return answer;
     }
 }
