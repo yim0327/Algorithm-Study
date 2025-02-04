cdb6656e817e626eaabf14874b4dd217a79e8d7e
재영 <yim020327@gmail.com>
2025-02-04 09:19:31 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,16 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        int ant = 5;
-        
-        for(int i = 0; i < 3; i++){
-            if(hp != 0){
-                answer += hp / ant;
-                hp = hp % ant;
-                ant -= 2;
-            }
-        }
-        
-        return answer;
+        return (hp/5) + (hp%5/3) + (hp%5%3);
     }
 }
