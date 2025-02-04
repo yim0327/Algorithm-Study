03b405ecf7c2360d68e1f13359059343b1cf7e1d
재영 <yim020327@gmail.com>
2025-02-04 10:13:20 +0900

    [level 0] Title: 가위 바위 보, Time: 0.56 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,21 +1,19 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
         for(int i = 0; i < rsp.length(); i++){
-            switch(rsp.charAt(i)){
-                case '2':
-                    answer += "0";
-                    break;
-                case '0':
-                    answer += "5";
-                    break;
-                case '5':
-                    answer += "2";
-                    break;
+            if(rsp.charAt(i) == '0'){
+                answer.append(5);
+            }
+            if(rsp.charAt(i) == '2'){
+                answer.append(0);
+            }
+            if(rsp.charAt(i) == '5'){
+                answer.append(2);
             }
         }
         
-        return answer;
+        return answer.toString();
     }
 }
