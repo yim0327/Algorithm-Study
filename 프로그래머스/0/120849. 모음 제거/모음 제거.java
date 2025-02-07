885105768d4ac2dcc7273f3093eb460d1bab1c19
재영 <yim020327@gmail.com>
2025-02-07 23:44:33 +0900

    [level 0] Title: 모음 제거, Time: 0.13 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.replace("a", "");
-        answer = answer.replace("e", "");
-        answer = answer.replace("i", "");
-        answer = answer.replace("o", "");
-        answer = answer.replace("u", "");
-        
-        return answer;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
