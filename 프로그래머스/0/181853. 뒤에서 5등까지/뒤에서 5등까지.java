4fa3e7df9fb17362aa55549e96be6fcc15efbf7f
재영 <yim020327@gmail.com>
2025-02-14 16:01:20 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.52 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -3,8 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        int[] answer = Arrays.copyOf(num_list, 5);
-        
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
