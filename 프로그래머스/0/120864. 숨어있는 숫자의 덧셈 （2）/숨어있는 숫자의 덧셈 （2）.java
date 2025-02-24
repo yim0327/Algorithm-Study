9aeef73b23371983597afb2a8224630389092e6d
재영 <yim020327@gmail.com>
2025-02-24 09:48:44 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 2.23 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(String my_string) {
-        String[] arr = my_string.split("\\D+");
-        
-        return Arrays.stream(arr)
+    public int solution(String my_string) {        
+        return Arrays.stream(my_string.replaceAll("[a-zA-Z]"," ").split(" "))
                     .filter(s-> !s.isEmpty())
                     .mapToInt(Integer::parseInt)
                     .sum();
