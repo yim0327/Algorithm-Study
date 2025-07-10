e4b9645155308f4e4ac7d06827db38f6e7d07635
재영 <yim020327@gmail.com>
2025-07-10 20:34:12 +0900

    [level 0] Title: A로 B 만들기, Time: 0.88 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,18 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(String before, String after) {
-        int answer = -1;
+        char[] a = before.toCharArray();
+        char[] b = after.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
         
-        for(int i = 0; i < before.length(); i++){
-            for(int j = 0; j < after.length(); j++){
-                if(before.charAt(i)==after.charAt(j)){
-                    after = after.replaceFirst(before.charAt(i)+"", " ");
-                    before = before.replaceFirst(before.charAt(i)+"", " ");
-                }
-            }
-        }
-        
-        after = after.replace(" ","");
-        answer = after.equals("") ? 1 : 0;
-        return answer;
+        return Arrays.equals(a, b) ? 1 : 0;
     }
 }
