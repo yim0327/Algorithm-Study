c049507137eefdb309365f651ca15c6347866304
재영 <yim020327@gmail.com>
2025-08-01 04:44:33 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/README.md
+++ 프로그래머스/0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921#) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 0.03 ms
+메모리: 86.6 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 01일 04:40:23
+2025년 08월 01일 04:44:32
 
 ### 문제 설명
 
--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,28 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        int idx = 0;
-        
-        for(int i = 0; i < A.length(); i++) {
-            if(A.charAt(0) == B.charAt(i)) {
-                idx = i;
-                
-                if(check(A, B, idx)) {
-                    return i;
-                }
-            }
-        }
-        
-        return -1;
-    }
-    
-    // 밀어서 만들 수 있는 지 체크하는 메서드.
-    public boolean check(String A, String B, int idx) {
-        for(int i = 1; i < A.length(); i++) {
-           idx++;
-           if(A.charAt(i) != B.charAt(idx%B.length()))
-               return false;
-        }
-        return true;
+        String tmpB = B.repeat(2);
+        return tmpB.indexOf(A);
     }
 }
