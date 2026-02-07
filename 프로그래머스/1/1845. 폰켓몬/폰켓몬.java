d4eafa5870cd30ac421d8c448b9f55f5fa57c2d8
재영 <yim020327@gmail.com>
2026-02-08 00:27:28 +0900

    [level 1] Title: 폰켓몬, Time: 4.14 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set set = new HashSet();
+        
+        for (int n : nums) {
+            set.add(n);
+        }
+        
+        return nums.length/2 < set.size() ? nums.length/2 : set.size();
+    }
+}
