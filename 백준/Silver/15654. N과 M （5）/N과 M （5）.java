110680539ee59cfbf153c7cf294cf259561c502d
재영 <yim020327@gmail.com>
2026-02-24 21:46:24 +0900

    [Silver III] Title: N과 M (5), Time: 272 ms, Memory: 28808 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    private static void permutation(int[] arr, int[] out, boolean[] visited, int depth, int r) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++) {
+                sb.append(out[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                out[depth] = arr[i];
+
+                permutation(arr, out, visited, depth + 1, r);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(s.nextToken());
+        int r = Integer.parseInt(s.nextToken());
+
+        s = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(s.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        permutation(arr, new int[r], new boolean[arr.length], 0, r);
+
+        System.out.print(sb.toString());
+    }
+}
