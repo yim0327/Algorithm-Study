ab3ccf56d723a774be03ccb1a477c2d2f3772bdb
재영 <yim020327@gmail.com>
2026-02-24 23:47:40 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    private static void combination(int[] arr, int[] out, int start, int depth, int r) {
+        if (depth == r) {
+            for (int i = 0; i < r; i++) {
+                sb.append(out[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int prev = Integer.MIN_VALUE;
+
+        for (int i = start; i < arr.length; i++) {
+            if (arr[i] == prev) continue;
+            prev = arr[i];
+
+            out[depth] = arr[i];
+            combination(arr, out, i+1, depth + 1, r);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer s = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(s.nextToken());
+        int r = Integer.parseInt(s.nextToken());
+
+        s = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(s.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        combination(arr, new int[r], 0, 0, r);
+
+        System.out.print(sb.toString());
+    }
+}
