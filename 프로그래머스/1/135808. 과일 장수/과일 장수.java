6b2f2e900f58bbd7bb5cedd4ac4a35894bba83bf
재영 <yim020327@gmail.com>
2026-04-22 16:00:49 +0900

    [level 1] Title: 과일 장수, Time: 63.14 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,15 +3,11 @@ import java.util.Arrays;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int idx = score.length - m;
         
         Arrays.sort(score);
         
-        for (int i = 0; i < score.length / m; i++) {
-            answer += score[idx] * m;
-            
-            if (idx - m < 0) break;
-            idx -= m;
+        for (int i = score.length - m; i >= 0; i-=m) {
+            answer += score[i] * m;
         }
         
         return answer;
