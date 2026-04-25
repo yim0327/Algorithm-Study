c9359f12bdffd2c0c97a6b08b5c3374ea5df41d6
재영 <yim020327@gmail.com>
2026-04-25 17:59:37 +0900

    [level 1] Title: 둘만의 암호, Time: 1.04 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#) 
 
 ### 성능 요약
 
-메모리: 81.5 MB, 시간: 31.57 ms
+메모리: 88.6 MB, 시간: 1.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 25일 17:47:46
+2026년 04월 25일 17:59:35
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,31 +1,18 @@
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder sb = new StringBuilder();
-        char[] srr = s.toCharArray();
         
-        // 한 자씩 보기
-        for (int i = 0; i < srr.length; i++) {
-            int num = (int) srr[i];
+        for (char c : s.toCharArray()) {
+            char tmp = c;
+            int cnt = 0;
             
-            // 인덱스만큼 점프
-            for (int j = 0; j < index; j++) {
-                num++;
-                
-                if (String.valueOf((char)num)
-                    .matches("["+skip+"]")) {
-                    j--;
-                }
-                
-                if (num > 'z') {
-                    num = num - ('z' - 'a' + 1);
-                    if (String.valueOf((char)num)
-                        .matches("["+skip+"]")) j--;
-                }
+            while (cnt < index) {
+                tmp = (tmp == 'z' ? 'a' : (char)(tmp+1));
+                if (!skip.contains(String.valueOf(tmp))) cnt++;
             }
             
-            char c = (char) num;
-            sb.append(c);
-        }
+            sb.append(tmp);
+        }        
         
         return sb.toString();
     }
