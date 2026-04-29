fb36803dcae6d969ee836f1f1139f50263116014
재영 <yim020327@gmail.com>
2026-04-29 15:25:19 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.64 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,11 +2,11 @@ import java.util.HashMap;
 
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        HashMap<String, Integer> map = new HashMap<>();
-        map.put("R", 0); map.put("T", 0);
-        map.put("C", 0); map.put("F", 0);
-        map.put("J", 0); map.put("M", 0);
-        map.put("A", 0); map.put("N", 0);
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('R', 0); map.put('T', 0);
+        map.put('C', 0); map.put('F', 0);
+        map.put('J', 0); map.put('M', 0);
+        map.put('A', 0); map.put('N', 0);
         
         for (int i = 0; i < survey.length; i++) {
             String s = survey[i];
@@ -14,34 +14,32 @@ class Solution {
             if (choices[i] == 1 || choices[i] == 2 
                 || choices[i] == 3) {
                 int point = 4 - choices[i];
-                String type = String.valueOf(s.charAt(0));
-                map.put(type, map.get(type) + point);
+                map.put(s.charAt(0), map.get(s.charAt(0)) + point);
             }
             else if ((choices[i] == 5 || choices[i] == 6 
                       || choices[i] == 7)) {
                 int point = choices[i] - 4;
-                String type = String.valueOf(s.charAt(1));
-                map.put(type, map.get(type) + point);
+                map.put(s.charAt(1), map.get(s.charAt(1)) + point);
             }
         }
         
         StringBuilder sb = new StringBuilder();
         
         sb.append(
-            Math.max(map.get("R"), map.get("T")) 
-            == map.get("R") ? "R" : "T"
+            Math.max(map.get('R'), map.get('T')) 
+            == map.get('R') ? 'R' : 'T'
         );
         sb.append(
-            Math.max(map.get("C"), map.get("F")) 
-            == map.get("C") ? "C" : "F"
+            Math.max(map.get('C'), map.get('F')) 
+            == map.get('C') ? 'C' : 'F'
         );
         sb.append(
-            Math.max(map.get("J"), map.get("M")) 
-            == map.get("J") ? "J" : "M"
+            Math.max(map.get('J'), map.get('M')) 
+            == map.get('J') ? 'J' : 'M'
         );
         sb.append(
-            Math.max(map.get("A"), map.get("N")) 
-            == map.get("A") ? "A" : "N"
+            Math.max(map.get('A'), map.get('N')) 
+            == map.get('A') ? 'A' : 'N'
         );
         
         return sb.toString();
