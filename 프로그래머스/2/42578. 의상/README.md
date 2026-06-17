2959429d20dfc1ae6bd061372feb6878991366ea
재영 <yim020327@gmail.com>
2026-06-17 15:54:19 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,6 +1,6 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 17일 15:54:08
+2026년 06월 17일 15:54:17
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,25 +2,18 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> type = new HashMap<>();
+        int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
         
         for (int i = 0; i < clothes.length; i++) {
-            if (type.containsKey(clothes[i][1])) {
-                type.put(clothes[i][1], type.get(clothes[i][1]) + 1);
+            if (map.containsKey(clothes[i][1])) {
+                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
             }
             else {
-                type.put(clothes[i][1], 1);
+                map.put(clothes[i][1], 1);
             }
         }
         
-        int sum = 0;
-        
-        for (int n : type.values()) {
-            sum += n;
-            answer *= (n+1);
-        }
-        
-        return --answer;
+        return answer;
     }
 }
