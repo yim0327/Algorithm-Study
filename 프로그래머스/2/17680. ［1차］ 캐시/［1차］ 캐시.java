deaae35592c594381d52a80ca6c1898903f0367b
재영 <yim020327@gmail.com>
2026-06-20 00:43:12 +0900

    [level 2] Title: [1차] 캐시, Time: 22.53 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,32 +1,30 @@
-import java.util.ArrayDeque;
+import java.util.LinkedHashSet;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-        ArrayDeque<String> queue = new ArrayDeque<>(cacheSize);
-        
         if (cacheSize == 0) return cities.length * 5;
-        
-        for (int i = 0; i < cities.length; i++) {
-            String s = cities[i].toLowerCase();
-            
-            if (queue.isEmpty()) {
+
+        LinkedHashSet<String> cache = new LinkedHashSet<>();
+        int answer = 0;
+
+        for (String city : cities) {
+            city = city.toLowerCase();
+
+            if (cache.contains(city)) {
+                cache.remove(city);
+                answer += 1;
+            } 
+            else {
+                if (cache.size() == cacheSize) {
+                    String oldest = cache.iterator().next();
+                    cache.remove(oldest);
+                }
                 answer += 5;
             }
-            else {
-                if (queue.contains(s)) {
-                    queue.remove(s);
-                    answer += 1;
-                }
-                else {
-                    if (queue.size() == cacheSize) queue.remove();
-                    answer += 5;
-                }
-            }
-            
-            queue.add(s);
+
+            cache.add(city);
         }
-                
+
         return answer;
     }
 }
