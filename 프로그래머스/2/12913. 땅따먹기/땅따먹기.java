774eb2886d4f80f1b034c8fefc5c3ab5fab378bf
재영 <yim020327@gmail.com>
2026-06-30 10:14:03 +0900

    [level 2] Title: 땅따먹기, Time: 9.77 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,26 +1,27 @@
 class Solution {
-    int solution(int[][] land) {
-        int len = land.length-1;
-
-        for (int i = 1; i < land.length; i++) {
-            for (int j = 0; j < 4; j++) {
-                int max = 0;
-                
-                for (int k = 0; k < 4; k++) {
-                    if (j == k) continue;
-                    max = max < land[i-1][k] ? land[i-1][k] : max;
-                }
-                
-                land[i][j] += max;
-            }
+    private int calculrate(int[][] land, int row, int pass) {
+        int max = 0;
+        
+        for (int i = 0; i < 4; i++) {
+            if (i == pass) continue;
+            max = Math.max(max, land[row-1][i]);
         }
         
+        return max;
+    }
+    
+    int solution(int[][] land) {
+        for (int i = 1; i < land.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                land[i][j] += calculrate(land, i, j);
+            }
+        }
+
+        int len = land.length - 1;
+
         return Math.max(
-                    Math.max(
-                        Math.max(land[len][0], land[len][1]),
-                        land[len][2]
-                    ),
-                    land[len][3]
-                );
+            Math.max(land[len][0], land[len][1]),
+            Math.max(land[len][2], land[len][3])
+        );
     }
 }
