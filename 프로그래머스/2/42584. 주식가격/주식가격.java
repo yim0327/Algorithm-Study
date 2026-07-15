04892a22793945a0bdf67de205ea4dc3294cea6e
재영 <yim020327@gmail.com>
2026-07-15 11:28:21 +0900

    [level 2] Title: 주식가격, Time: 13.84 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,28 +1,26 @@
-import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         
-        // stack 초기화
-        for (int p : prices) stack.add(p);
+        stack.push(0);
         
-        int pre_num = 0;
-        int second = 0;
-        
-        for (int i = prices.length-1; i >= 0; i--) {
-            if (pre_num == 0) {
-                pre_num = stack.pop();
-                answer[i] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // '현재값보다 작은 경우'일 시,
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int pre_idx = stack.pop();      // 인덱스 꺼내기
+                answer[pre_idx] = i - pre_idx;  // 기간 계산
             }
-            
-            second++;
-            answer[i] = second;
-            
-            int tmp = stack.pop();
-            
-            if (tmp > pre_num) second = 0;
+            stack.push(i);
+        }
+        
+        // 남은 '현재값보다 크거나 같은 경우' 계산
+        while (!stack.isEmpty()) {
+            int pre_idx = stack.pop();
+            answer[pre_idx] = prices.length - pre_idx -1;
         }
         
         return answer;
