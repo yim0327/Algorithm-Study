c6477676809bb002c74263d437607a7248d2cd9e
재영 <yim020327@gmail.com>
2026-07-21 14:13:59 +0900

    [level 2] Title: 전화번호 목록, Time: 222.44 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,11 +3,9 @@ import java.util.Arrays;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        
+                
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i+1].indexOf(phone_book[i]) == 0) {
-                return false;
-            }
+            if (phone_book[i+1].indexOf(phone_book[i]) == 0) return false;
         }
         
         return true;
